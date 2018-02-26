f4090a482e3c3df8eb266816c2fc2753a38fd9f3
vagrant <vagrant@homestead>
2018-02-26 11:04:14 +0000

    临时提交--增加数据库前缀

--- resources/views/layouts/xianchang.blade.php
+++ resources/views/layouts/xianchang.blade.php
@@ -8,17 +8,14 @@
     <link href="{{ asset('css/xianchang.cat.css') }}" rel="stylesheet">
 </head>
 <body>
-
 <div class="g-f" id="hd">
     <h2 class="group-location">所在位置：抽奖1组<a href="javascript:void(0);" id="updateSelect">修改</a></h2>
     <h1 id="logo"><a href="/" title="返回首页">中国婚博会现场系统</a></h1>
-
     <span class="g-f-r name">欢迎你，<a href="/user/chpwd">xiehaojie</a></span>
     <a class="g-f-r logout" href="/">退出</a>
-
 </div>
 <div id="container">
-
+    
 </div>
 <div id="ft">
     2008-{{date('Y')}}&nbsp;&copy;&nbsp;www.jiehun.com.cn
