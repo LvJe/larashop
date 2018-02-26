9adeb5efad22dcbd6c3e00bec61703b83645168a
vagrant <vagrant@homestead>
2018-02-26 08:54:36 +0000

    伟照现场

---
+++ resources/views/layouts/xianchang.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=7; IE=8; IE=9; IE=10" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
+    <title>中国婚博会现场系统</title>
+    <link href="{{ asset('css/xianchang.cat.css') }}" rel="stylesheet">
+</head>
+<body>
+
+<div class="g-f" id="hd">
+    <h2 class="group-location">所在位置：抽奖1组<a href="javascript:void(0);" id="updateSelect">修改</a></h2>
+    <h1 id="logo"><a href="/" title="返回首页">中国婚博会现场系统</a></h1>
+
+    <span class="g-f-r name">欢迎你，<a href="/user/chpwd">xiehaojie</a></span>
+    <a class="g-f-r logout" href="/">退出</a>
+
+</div>
+<div id="container">
+
+</div>
+<div id="ft">
+    2008-{{date('Y')}}&nbsp;&copy;&nbsp;www.jiehun.com.cn
+    <br/><a href="/software/" style="color:#CCC;">download</a>
+</div>
+</body>
+</html>
